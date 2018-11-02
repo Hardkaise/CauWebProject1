21f2e18dffbf1b99fc33522e620d0989cacf9a2a
Clément Nancelle <clement.nancelle@epitech.eu>
2018-11-02 17:35:32 +0900

    calculator finished

--- js/calculator.js
+++ js/calculator.js
@@ -2,23 +2,57 @@ class Calculator {
 
     constructor() {
         this.refInput = document.querySelector("#calculator .head-calculator input");
+        this.history = [];
+        this.posHistory = -1;
     }
 
     listOnDigit() {
         const digit = document.querySelectorAll("#calculator .body-calculator .calculator-digit .button");
         const input = this.refInput;
 
-        console.log(this.refInput);
         digit.forEach(d => d.addEventListener("click", () => input.value += d.getAttribute("data-value")));
-        console.log(digit);
+    }
+
+    listOnOperator() {
+        const operator = document.querySelectorAll("#calculator .body-calculator .calculator-operator .button");
+        const input = this.refInput;
+
+        operator.forEach(d => d.addEventListener("click", () => input.value += d.getAttribute("data-value")));
+    }
+
+    onEqualClick() {
+        const result =  eval(this.refInput.value);
+        this.history.push(result);
+        this.refInput.value = result;
+        this.posHistory += 1;
+        console.log(this.history.length);
+    }
+
+    clearInput() {
+        this.refInput.value = "";
+    }
+
+    eraseInput() {
+        this.refInput.value = this.refInput.value.slice(0, -1);
+    }
+
+    upHistory() {
+       if (this.posHistory < this.history.length - 1) {
+           this.posHistory += 1;
+           this.refInput.value = this.history[this.posHistory];
+       }
+    }
+
+    downHistory() {
+        if (this.posHistory > 0) {
+            this.posHistory -= 1;
+            this.refInput.value = this.history[this.posHistory];
+        }
     }
 
     run() {
-        this.listOnDigit()
-    }
-
-    calcul() {
-
+        this.listOnDigit();
+        this.listOnOperator();
     }
 }
 
